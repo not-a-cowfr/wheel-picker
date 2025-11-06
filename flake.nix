fe1f4149c281ca54e4ff3191be7473b4d3445403
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-11-05 17:11:27 -0800

    format

--- flake.nix
+++ flake.nix
@@ -4,23 +4,30 @@
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            packages = [
-              # required for rust-analyzer
-              rust-analyzer
-            ];
-          };
-        }
-      );
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            # required for rust-analyzer
+            openssl
+            pkg-config
+          ];
+        };
+      }
+    );
 }
